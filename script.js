20b173a32c729be041c39a5af4d8ced3f1d2ec81
bukola4tech <bukolaaishaabdullahi@gmail.com>
2022-11-22 01:48:39 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log(vowel_count(abolf));
+
+
+function countwords(str) {
+    const arr=str.split('');
+
+    return arr.filter(word=> word i=='').length;
+}
+
+console.log(countwords('one two three'));
+console.log(countwords('this is a long string'));
+
+console.log('hello world'.split(''));
+
+
+console.log('one two three'spilt(''));
+
+console.log('hello world' .split(''));
+
+
+console.log('one two three .split(''));
